9d6e8f49652bc1eac504b9dab9513b9d2bf9f5cc
Paul Skains <pdskains@gmail.com>
2017-07-17 16:47:05 -0500

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,24 @@
+//problem 1
+//
+// for(let i=1; i <= 100; i++){
+//   if((i % 3) === 0 && (i % 5) === 0){
+//       console.log('FizzBuzz');
+// }else if((i % 3) === 0){
+//       console.log('Fizz');
+// }else if((i % 5) === 0){
+//       console.log('Buzz');
+// }else{
+//       console.log(i);
+// }
+// };
+//
+//problem 2
+//
+// let array = [1,1,1,1,1,1,1,1,1,2,4,5,6,7,8,8,1,1,1,1,1,3];
+//
+// function averageArray(){
+//   console.log(Math.floor(array.length));
+// };
+//
+// averageArray(array);
+//
